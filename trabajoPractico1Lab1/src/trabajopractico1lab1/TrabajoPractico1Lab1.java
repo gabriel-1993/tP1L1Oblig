dbce433ca9d2263f6dd6bca66fa58a894311f559
Gabriel Torrez <110358252+gabriel-1993@users.noreply.github.com>
2023-08-15 10:25:26 -0300

    primerPushDelTp1Obligatorio

---
+++ trabajoPractico1Lab1/src/trabajopractico1lab1/TrabajoPractico1Lab1.java
@@ -0,0 +1,27 @@
+
+package trabajopractico1lab1;
+
+/**
+Ejercicio  (Individual):
+
+Crea un nuevo repositorio en GitHub: Ve a la página principal de GitHub (github.com) y crea un nuevo repositorio vacío. Puedes darle un nombre y una descripción según tus preferencias.
+Instala GitHub Desktop: Descarga e instala la aplicación GitHub Desktop en tu computadora desde el sitio web oficial (desktop.github.com). Sigue las instrucciones de instalación proporcionadas.
+Clona el repositorio en tu computadora: Abre GitHub Desktop y selecciona la opción "Clone a repository" (Clonar un repositorio) en la pestaña "File" (Archivo). Elige el repositorio que creaste en el paso 1 y selecciona una ubicación en tu computadora para clonarlo.
+Realiza cambios en el repositorio: Abre el repositorio clonado en tu explorador de archivos. Agrega un archivo nuevo o realiza modificaciones en un archivo existente. Por ejemplo, puedes crear un archivo de texto simple llamado "ejercicio.txt" y agregar algún contenido.
+Confirma y guarda los cambios: Vuelve a GitHub Desktop y verás los cambios detectados en el repositorio clonado. Escribe un resumen breve y descriptivo en el campo "Summary" (Resumen) y opcionalmente, añade una descripción más detallada en el campo "Description" (Descripción). Luego, selecciona el botón "Commit to main" (Confirmar en rama principal) para guardar los cambios localmente.
+Sincroniza con el repositorio remoto: Una vez que hayas confirmado los cambios, selecciona la opción "Push origin" (Enviar a origen) en la parte superior de la interfaz de GitHub Desktop. Esto enviará los cambios al repositorio remoto en GitHub.
+Verifica los cambios en GitHub: Abre el repositorio en GitHub.com y verifica que los cambios realizados en tu computadora se reflejen en el repositorio remoto.
+Este ejercicio te ayudará a familiarizarte con los conceptos básicos de clonar un repositorio, realizar cambios, confirmarlos y sincronizarlos con el repositorio remoto utilizando GitHub Desktop. ¡Diviértete practicando!
+
+Abierta hasta : 31/08/2023 23:59
+ */
+public class TrabajoPractico1Lab1 {
+
+   
+    public static void main(String[] args) {
+        // Agrego comentarios y la consigna para tener cambios en el repo clonado...
+        //commit
+        //push...
+    }
+    
+}
